58bb027aad38d54f7f4a3e6f2465c8aa7cd18efa
Nexusrisen <bdkohn91@gmail.com>
2018-09-06 14:20:56 -0400

    Added DOJ Like Vehicles

---
+++ FXServer/resources/[vehicles]/los_santos_pd/data/vehicle_names.lua
@@ -0,0 +1,34 @@
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	-- cvpi w/ liberty
+	AddTextEntry('0x54830233', 'PD')
+	-- 2015 charger
+	AddTextEntry('0x519EF9F7', 'PD2')
+	-- 2014 explorer
+	AddTextEntry('0x43605D7A', 'PD3')
+	-- 2014 slicktop explorer
+	AddTextEntry('0x61A019F9', 'PD4')
+	-- ford expedition
+	AddTextEntry('0xD3487D4C', 'PD5')
+	-- 2016 explorer
+	AddTextEntry('0x7C264F05', 'PD6')
+	-- cvpi w/ vector
+	AddTextEntry('0x65E42281', 'PD7')
+	-- 2009 charger
+	AddTextEntry('0x9EB8142C', 'PD8')
+	-- ford f350
+	AddTextEntry('0xFA81CBBE', 'PD9')
+	-- cvpi slicktop
+	AddTextEntry('0x7DB7A427', 'PD10'
+     -- cvpi slicktop
+	AddTextEntry('0x7DB7A427', 'PD11')
+     -- cvpi slicktop
+	AddTextEntry('0x7DB7A427', 'PD12')
+     -- cvpi slicktop
+	AddTextEntry('0x7DB7A427', 'PD13')
+     -- cvpi slicktop
+	AddTextEntry('0x7DB7A427', 'PD14')
+
+
